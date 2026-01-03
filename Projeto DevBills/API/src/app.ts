50b6e8ef065023ba4142653d39ae9d72172c00f3
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2026-01-03 01:22:57 -0300

    doing the first route

--- Projeto DevBills/API/src/app.ts
+++ Projeto DevBills/API/src/app.ts
@@ -1,6 +1,13 @@
 import Fastify from "fastify";
+import type { FastifyInstance } from "fastify";
+import routes from './routes/index.js';
 
 
-const app = Fastify()
+const app:FastifyInstance = Fastify({
+    logger: true
+})
+
+//Plugins
+app.register(routes)
 
 export default app
--- Projeto DevBills/API/src/routes/index.ts
+++ Projeto DevBills/API/src/routes/index.ts
@@ -1,7 +1,13 @@
 import type { FastifyInstance } from "fastify"
 
-async function routes(fastify: FastifyInstance) {
+async function routes(fastify: FastifyInstance): Promise<void> {
+    
     fastify.get('/health', async() => {
-        
+        return {
+            status: 'ok',
+            message: 'DevBills API rodando normalmente'
+        }
     })
 }
+
+export default routes

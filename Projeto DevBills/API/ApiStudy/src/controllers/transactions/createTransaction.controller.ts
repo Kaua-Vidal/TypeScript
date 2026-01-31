059f723f89df28bb131e431023cfaf1c2667d2d1
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2026-01-30 23:28:17 -0300

    creating the transaction route

---
+++ Projeto DevBills/API/ApiStudy/src/controllers/transactions/createTransaction.controller.ts
@@ -0,0 +1,7 @@
+import type { FastifyReply, FastifyRequest } from "fastify"
+
+const createTransaction = async(request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+
+}
+
+export default createTransaction
--- Projeto DevBills/API/ApiStudy/src/routes/index.ts
+++ Projeto DevBills/API/ApiStudy/src/routes/index.ts
@@ -1,5 +1,6 @@
 import type { FastifyInstance } from "fastify"
 import categoryRoutes from "./category.routes"
+import transactionRoutes from "./transaction.routes"
 
 
 async function routes(fastify: FastifyInstance): Promise<void> {
@@ -10,7 +11,8 @@ async function routes(fastify: FastifyInstance): Promise<void> {
         }
     })
 
-    fastify.register(categoryRoutes, { prefix: '/categories'})
+    fastify.register(categoryRoutes, { prefix: '/categories' })
+    fastify.register(transactionRoutes, { prefix: "/transaction" })
 }
 
 export default routes

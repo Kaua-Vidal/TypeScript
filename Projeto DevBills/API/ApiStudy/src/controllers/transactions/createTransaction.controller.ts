207413b0a3f488fc81f04fa4fed60ec3d7a83c19
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2026-02-04 23:36:35 -0300

    creating the transaction route

--- Projeto DevBills/API/ApiStudy/src/controllers/transactions/createTransaction.controller.ts
+++ Projeto DevBills/API/ApiStudy/src/controllers/transactions/createTransaction.controller.ts
@@ -1,7 +1,13 @@
 import type { FastifyReply, FastifyRequest } from "fastify"
 
 const createTransaction = async(request: FastifyRequest, reply: FastifyReply): Promise<void> => {
+    const userId = "1234aaa" // Vem do request.userId
 
+    if (!userId) {
+        reply.status(401).send({ error: "Usuário não autenticado"})
+    }
+
+    // Validação dos Dados
 }
 
 export default createTransaction

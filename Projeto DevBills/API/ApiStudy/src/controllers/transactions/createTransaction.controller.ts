919abfc00a3d2ddc5a20b90606a64f32ac838e27
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2026-02-06 18:36:19 -0300

    created Transaction Controller

--- Projeto DevBills/API/ApiStudy/src/controllers/transactions/createTransaction.controller.ts
+++ Projeto DevBills/API/ApiStudy/src/controllers/transactions/createTransaction.controller.ts
@@ -1,13 +1,62 @@
 import type { FastifyReply, FastifyRequest } from "fastify"
+import { createTransactionSchema } from "../../schemas/transaction.schema"
+import prisma from "../../config/prisma"
 
 const createTransaction = async(request: FastifyRequest, reply: FastifyReply): Promise<void> => {
     const userId = "1234aaa" // Vem do request.userId
 
     if (!userId) {
-        reply.status(401).send({ error: "Usuário não autenticado"})
+        return reply.status(401).send({ error: "Usuário não autenticado"})
     }
 
     // Validação dos Dados
+
+    const result = createTransactionSchema.safeParse(request.body)
+
+    if(!result.success) {
+        const errorMessage = result.error.errors[0].message 
+            || "Validação Inválida"
+        reply.status(500).send({ error: errorMessage})
+        return;
+    }
+
+    const transaction = result.data
+
+    try {
+        const category = await prisma.category.findFirst({ 
+            where:{
+                id: transaction.categoryId,
+                type: transaction.type
+            }
+        })
+
+        if (!category) {
+            reply.status(400).send({ error: "Categoria inválida"})
+            return;
+        }
+
+        //Corrigindo data de String -> Date
+        const parsedDate = new Date(transaction.date)
+        
+        //Enviar info para Banco de Dados
+        const newTransaction = await prisma.transaction.create({
+            data: {
+                ...transaction,
+                userId,
+                date: parsedDate
+            },
+            //Incluindo as infos de Categorias também
+            include: {
+                category: true
+            }
+        })
+
+
+        reply.status(201).send(newTransaction)
+    } catch(err) {
+        request.log.error("Erro ao criar transação")
+        reply.status(500).send({ error: "Erro interno do servidor" })
+    }
 }
 
 export default createTransaction
--- Projeto DevBills/API/ApiStudy/src/schemas/transaction.schema.ts
+++ Projeto DevBills/API/ApiStudy/src/schemas/transaction.schema.ts
@@ -2,9 +2,9 @@ import { z } from 'zod';
 import { ObjectId } from 'mongodb';
 import { TransactionType } from '@prisma/client';
 
-const isValidObjectId = (id: string): boolean => ObjectId.isValid()
+const isValidObjectId = (id: string): boolean => ObjectId.isValid(id)
 
-export const createTransaction = z.object ({
+export const createTransactionSchema = z.object ({
     description: z.string().min(1, "Descrição obrigatória"),
     amount: z.number().positive("O número deve ser um valor positivo"),
     date: z.coerce.date({
@@ -13,7 +13,7 @@ export const createTransaction = z.object ({
     categoryId: z.string().refine(isValidObjectId, {
         message: "Categoria inválida"
     }),
-    type: z.enum([TransactionType.expense, TransactionType.income]), {
+    type: z.enum([TransactionType.expense, TransactionType.income], {
         errorMap: () => ({ message: "Data inválida"})
-    }
+    })
 })

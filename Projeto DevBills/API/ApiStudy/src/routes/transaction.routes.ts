85ba901f8ce87ce597ea64b754b7435dc61dbd9c
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2026-02-06 19:19:48 -0300

    putting the schema on the route

--- Projeto DevBills/API/ApiStudy/src/routes/transaction.routes.ts
+++ Projeto DevBills/API/ApiStudy/src/routes/transaction.routes.ts
@@ -1,12 +1,17 @@
 import type { FastifyInstance } from "fastify"
 import createTransaction from "../controllers/transactions/createTransaction.controller"
+import { zodToJsonSchema} from "zod-to-json-schema"
+import { createTransactionSchema } from "../schemas/transaction.schema"
+
+// @ts-expect-error: complex type instantiation
+const transactionJsonSchema = zodToJsonSchema(createTransactionSchema);
 
 const transactionRoutes = async(fastify: FastifyInstance) => {
     fastify.route({
         method: "POST",
         url: "/",
         schema: {
-            
+            body: transactionJsonSchema
         },
         handler: createTransaction
     })

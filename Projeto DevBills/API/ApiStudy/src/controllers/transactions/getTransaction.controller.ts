f4ae4ed8fc526377f4dfafae6557a25e792d4840
Kaua-Vidal <kaua.vidal@dcx.ufpb.br>
2026-02-10 23:17:45 -0300

    creating the controller

---
+++ Projeto DevBills/API/ApiStudy/src/controllers/transactions/getTransaction.controller.ts
@@ -0,0 +1,7 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+
+export const getTransactions = async(
+    request: FastifyRequest<{Querystring: }>, 
+    reply: FastifyReply): Promise<void> => {
+
+}
